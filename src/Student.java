c5f50ba108aab8afce3038d381ebb7ac3499c36e
liwei <7274561@qq.com>
2021-01-10 16:51:08 +0800

    Initial commit

---
+++ src/Student.java
@@ -0,0 +1,30 @@
+public class Student extends User {
+
+    /*
+    成员变量
+     */
+    private String major;
+    private String phone;
+
+    public Student(String major, String phone) {
+
+        this.major = major;
+        this.phone = phone;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ src/Teacher.java
@@ -0,0 +1,12 @@
+public class Teacher extends User{
+
+    private String zhiCheng;
+
+    public String getZhiCheng() {
+        return zhiCheng;
+    }
+
+    public void setZhiCheng(String zhiCheng) {
+        this.zhiCheng = zhiCheng;
+    }
+}
